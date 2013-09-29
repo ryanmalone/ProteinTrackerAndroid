565e965ae5dc61033f3b7400373e42a6233bba99
ryan.malone@blondgorilla.com <ryan.malone@blondgorilla.com>
2013-09-29 15:15:09 +0800

    Finished android
    Need to style but basically done.

--- MainActivity.cs
+++ MainActivity.cs
@@ -5,13 +5,20 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using ServiceStack.ServiceClient.Web;
+using ProteinTrackerMVC.api;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProteinTrackerAndroid
 {
 	[Activity (Label = "Protein Tracker", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
+		private JsonServiceClient client;
+		private IList<User> users;
 
+		 
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -20,7 +27,95 @@ namespace ProteinTrackerAndroid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
+			client = new JsonServiceClient ("http://10.1.1.109/api");
+
+
+			PopulateSelectUsers ();
+
+			Spinner usersSpinner = FindViewById<Spinner> (Resource.Id.usersSpinner);
+			usersSpinner.ItemSelected += (object sender, AdapterView.ItemSelectedEventArgs e) => 
+			{
+				var selectedUser = users[usersSpinner.SelectedItemPosition];
+				TextView goalTextView = FindViewById<TextView> (Resource.Id.GoalTotal);
+				TextView totalTextView = FindViewById<TextView> (Resource.Id.Total);
+
+				goalTextView.Text = selectedUser.Goal.ToString();
+				totalTextView.Text = selectedUser.Total.ToString();
+
+
+			};
+
+			// event for add user
+		var addUserButton = FindViewById<Button> (Resource.Id.AddUser);
+			addUserButton.Click += (object sender, EventArgs e) => {
+				TextView goalTextView = FindViewById<TextView> (Resource.Id.Goal);
+				TextView nameTextView = FindViewById<TextView> (Resource.Id.Name);
+				var goal = int.Parse (goalTextView.Text);
+				var response = client.Send (new AddUser { Goal = goal, Name = nameTextView.Text });
+
+				PopulateSelectUsers ();
+				goalTextView.Text = string.Empty;
+				nameTextView.Text = string.Empty;
+
+				Toast.MakeText (this, "Added new user", ToastLength.Short).Show();
+
+
+			};
+
+			// event for add Protein
+			var addProteinButton = FindViewById<Button> (Resource.Id.AddProtein);
+			addProteinButton.Click += (object sender, EventArgs e) => {
+				TextView amountTextView = FindViewById<TextView> (Resource.Id.Amount);
+				var amount = int.Parse(amountTextView.Text);
+				var selectedUser = users[usersSpinner.SelectedItemPosition];
+
+				var response = client.Send (new AddProtein { Amount = amount, UserID = selectedUser.Id });
+				selectedUser.Total = response.NewTotal;
+				TextView totalTextView = FindViewById<TextView> (Resource.Id.Total);
+				totalTextView.Text = selectedUser.Total.ToString();
+				amountTextView.Text = string.Empty;
+
+				//string usermessage =  "Added Protein to :" & selectedUser.Name;
+
+				Toast.MakeText (this, "Added Protein to :" + selectedUser.Name, ToastLength.Short).Show();
+
+
+			};
+
+	
+
+			client.GetAsync<UserResponse>("users",(w2) => {
+				Console.WriteLine(w2);
+				foreach(var c in w2.Users) {
+					Console.WriteLine(c);
+					Console.WriteLine(c.Name);
+
+				}
+			}, 
+			(w2, ex) => {
+				Console.WriteLine(ex.Message);
+			});
 		
+		}
+
+		void PopulateSelectUsers ()
+		{
+
+		
+
+			var response = client.Get<UserResponse> ("users");
+
+				users = response.Users.ToList ();
+
+				var names = users.Select (u => u.Name);
+
+				var usersSpiner = FindViewById<Spinner> (Resource.Id.usersSpinner);
+				usersSpiner.Adapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1, names.ToArray());
+			
+						
+
+
+
 		}
 	}
 }
--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -60,8 +60,38 @@ namespace ProteinTrackerAndroid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050009
+			public const int AddProtein = 2131034121;
+			
+			// aapt resource value: 0x7f050007
+			public const int AddUser = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int Amount = 2131034120;
+			
+			// aapt resource value: 0x7f050006
+			public const int Goal = 2131034118;
+			
+			// aapt resource value: 0x7f05000b
+			public const int GoalTotal = 2131034123;
+			
+			// aapt resource value: 0x7f050005
+			public const int Name = 2131034117;
+			
+			// aapt resource value: 0x7f05000a
+			public const int Total = 2131034122;
+			
+			// aapt resource value: 0x7f050003
+			public const int linearLayout1 = 2131034115;
+			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView3 = 2131034116;
 			
 			// aapt resource value: 0x7f050001
 			public const int usersSpinner = 2131034113;
